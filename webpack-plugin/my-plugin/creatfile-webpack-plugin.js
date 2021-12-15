45bb91e9c8099c4cc1ae305be059257d1b9b3987
wangliran3 <wangliran3@jd.com>
2021-12-15 17:20:33 +0800

    chore:practice write plugin

--- webpack-plugin/my-plugin/creatfile-webpack-plugin.js
+++ webpack-plugin/my-plugin/creatfile-webpack-plugin.js
@@ -1,10 +1,18 @@
 class CreatfileWebpackPlugin{
-	constructor() {
+	constructor(option) {
+		// option 可以获取到插件的配置
 		console.log('This is my webpack plugin')
 	}
 
-	apply(compiler) {
-		
+	apply(compiler) {		
+		compiler.hooks.emit.tapAsync('CreatfileWebpackPlugin', (compilation, callback) => {
+			// 查看compilation里面的内容 可以借助node的断点调试
+			// debugger;
+			compilation.assets['my-new-file.txt'] = {
+				source: () => 'I can write a plugin'
+			}
+			callback();
+		});
 	}
 }
 
